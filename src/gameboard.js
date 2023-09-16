6487aa34e0087080e8f523b924114ba7b1d55c22
TheMonist <rogerrosena@gmail.com>
2023-09-16 10:23:23 -0400

    another attempt fixing gameboard module

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,171 +1,52 @@
-import Ship from './ship';
-import convertCoordinatesToIndex from './utils';
-
-/* eslint-disable no-use-before-define */
-function Gameboard() {
-    const cells = generateCells(); 
-    const ships = [];
-    const misses = [];
-
-    function generateCells() {
-        const cells = [];
-    
-        // Creating each row
-        for(let i = 0; i < 10; i+=1) {
-            // Creating each col    
-            for(let j = 0; j< 10; j+=1) {
-                // create the cell with the right dataset elements. 
-                const cell = {
-                    x: i,
-                    y: j,
-                    occupied: false,
-                    attempted: false,
-                    occupyCell() {
-                        this.occupied = true;
-                    },
-                    markAsAttempted() {
-                        this.attempted = true;
-                    },
-                }
-                cells.push(cell);
-            }
-        }
-    
-        return cells;
-    }
-
-    function getCells() {
-        return cells;
-    }
-
-    function getShips() {
-        return ships;
-    }
-
-    function getMisses() {
-        return misses;
-    }
-
-    function shipFits(startCell, size, orientation) {
-        if (orientation === 'h') {
-            // Horizontal fit: col index at start + size should be less than final col index
-            if (!(startCell.y + (size -1) <= 9)) return false;
-
-            // Check if any cell in the way is occupied
-            for(let i = startCell.y; i < startCell.y + size; i+=1) {
-                const index = convertCoordinatesToIndex(startCell.x, i);
-                if (cells[index].occupied === true) return false;
-            }
-            // Otherwise: 
-            return true;
-        }
-        else if (orientation === 'v') {
-            // Check for board overflow at the bottom.
-            if((startCell.x + (size -1) > 9)) return false;
-
-            // Check if any cell in the way is occupied
-            for(let i = startCell.x; i < startCell.x + size; i+=1) {
-                const index = convertCoordinatesToIndex(i, startCell.y);
-                if (cells[index].occupied === true) return false;
-            }
-
-            // Otherwise: 
-            return true;
-        }
-        // Error is only thrown if orientation is neither h nor v
-        throw new Error('Orientation value invalid. Format = "h" or "v"');
-        
-    };
-
-    function getCellFromCoords(coords) {
-        return cells[convertCoordinatesToIndex(coords.x, coords.y)];
-    }
-    function attemptPlaceShip(startCellCoordinates, size, orientation) {
-        // Get cells from coords
-        const startCell = getCellFromCoords(startCellCoordinates);
-        
-        // Checks if ship fits; if not, early return.
-        if (shipFits(startCell, size, orientation) === false) 
-        return false;
-
-        // For ship constructor
-        let cellIndices = [];
-        
-        if (orientation === 'h') {
-            // Occupy the right cells
-            for(let i = startCell.y; i < startCell.y + size; i+=1) {
-                // Reconstitutes index from position
-                const index = convertCoordinatesToIndex(startCell.x, i);
-                cells[index].occupyCell(); 
-                cellIndices.push(index);
-            }
-            // Store ship in array
-            ships.push(Ship(cellIndices));
-
-            // Indicates success
-            return true;
-        } else if (orientation === 'v') {
-            // Occupy the right cells
-            for(let i = startCell.x; i < startCell.x + size; i+=1) {
-                // Reconstitutes index from position
-                const index = convertCoordinatesToIndex(i, startCell.y);
-                cells[index].occupyCell(); 
-                cellIndices.push(index);
-            }
-            // Store ship in array
-            ships.push(Ship(cellIndices));
-
-            // Indicates success
-            return true;
-        }
-        // Error is only thrown if orientation is neither h nor v
-        throw new Error('Orientation value invalid. Format = "h" or "v"');
-    };
-
-    function receiveAttack(cellCoordinates) {
-        // Get cell from coords
-        const cell = getCellFromCoords(cellCoordinates);
-        
-        // Early returns if this position has already been targeted.
-        if (cell.attempted === true) return 'already attempted';
-        cell.markAsAttempted();
-        checkForHit(cells.indexOf(cell));
-    };
-    function checkForHit(cellIndex) {
-        // Checks if there is a ship at that position
-        for (let i = 0; i < ships.length; i+=1) {
-            // Tries to find a match among each ship's positions
-            for(let j = 0; j < ships[i].positions.length; j+=1) {
-                if (ships[i].positions[j] === cellIndex) {
-                    // Make ship take the hit
-                    ships[i].hit(cellIndex);
-                    // Return true
-                    return true;
-                }
-            }
-        }
-        // If no hit was registered
-        misses.push(cellIndex);
-        return false;
-    };
-    function allSunk() {
-        if (ships.length > 0) {
-            return ships.every(ship => ship.isSunk());
-        }
-        // Will return false if no ships are placed. 
-        return false;
-    }
-
-    // PUBLIC
+function Gameboard(player) {
     return {
-        getCells,
-        getCellFromCoords,
-        getShips,
-        getMisses,
-        attemptPlaceShip,
-        receiveAttack,
-        allSunk,
-    }
+        player: player,
+        ships: [],
+        misses: [],
 
-}
-export default Gameboard;
+        placeShip(ship, [x, y]) {
+            ship.startCoord = [x, y];
+
+            if (ship.direction === 'horizontal') ship.endCoord = [(x + (ship.length - 1)), y]
+            else ship.endCoord = [x, (y - (ship.length - 1))];
+
+            this.ships.push(ship);
+
+            return ship.endCoord;
+        },
+
+        receiveAttack([x, y]) {
+            let hit = false;
+
+            for(let i = 0; i < this.ships.length; i++) {
+                if(this.ships[i].direction === 'horizontal') {
+                    if(y === this.ships[i].startCoord[1] && x >= this.ships[i].startCoord[0] && x <= this.ships[i].endCoord[0]) {
+                        this.ships[i].hit();
+                        hit = true;
+
+                        this.ships[i].isSunk();
+                    }
+                }
+
+                if (this.ships[i].direction === 'vertical') {
+                    if (x == this.ships[i].startCoord[0] && y <= this.ships[i].startCoord[1] && y >= this.ships[i].endCoord[1]){
+                        this.ships[i].hit();
+                        hit = true;
+
+                        this.ships[i].isSunk()
+                    }
+                }
+            }
+
+            if (hit === false) this.misses.push([x, y]);
+            return hit;
+        },
+
+        allSunk() {
+            return this.ships.every(ship => ship.sunk === true)
+        }
+
+    }
+};
+
+module.exports = Gameboard
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,174 +1,23 @@
-import Gameboard from '../gameboard';
-import Ship from '../ship';
+const Gameboard = require('../gameboard');
 
-let testBoard;
+let board1 = Gameboard(1)
+board1.placeShip(4, [2, 3]);
+board1.receiveAttack([2, 3]);
+board1.receiveAttack([4, 3]);
+board1.receiveAttack([5, 3]);
 
-    beforeEach(() => {
-        testBoard = Gameboard();
-    });
-
-describe('Gameboard generation', () => {
-
-    test('A 100-cell board is created', () => {
-        expect(testBoard.getCells().length).toBe(100);
-    });
-
-    test('Cells have x and y coordinates', () => {
-        expect(testBoard.getCells().every(cell => cell.hasOwnProperty('x') && cell.hasOwnProperty('y')))
-        .toBe(true);
-    });
-
-    test('Each cell is an object with an initially "false" occupied condition', () => {
-        expect(testBoard.getCells()[0].occupied).toBe(false);
-    });
-    
-    test('Each cell has an "attempted" property which is initialized as false', () => {
-        expect(testBoard.getCells().every(cell => cell.attempted === false))
-        .toBe(true);
-    });
-
+test('Calculate end coordinates', () => {
+    expect(board1.placeShip(2, [1, 1])).toEqual([2, 1]);
 });
 
-describe('Ship placement', () => {
-
-    // shipFits was hidden
-    /* test('Function tests whether a ship will fit horizontally based on size', () => {
-        expect(testBoard.shipFits(testBoard.cells[6], 4, 'h')).toBe(true);
-    });
-    test('Ship will not fit horizontally if there is not enough space', () => {
-        expect(testBoard.shipFits(testBoard.cells[9], 4,'h')).toBe(false);
-    }); */
-
-    test('attemptPlaceShip returns false if there is no room (horizontally)', () => {
-        expect(testBoard.attemptPlaceShip(testBoard.getCells()[9], 4, 'h')).toBe(false);
-    });
-
-    test('Ship gets placed horizontally on coordinates if there is room: cells become occupied', () => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[6], 4, 'h');
-        let testedCells = [];
-        for(let i = 6; i < 10; i++) {
-            testedCells.push(testBoard.getCells()[i]);
-        }
-        expect(testedCells.every(cell => cell.occupied === true)).toBe(true);
-    });
-
-    test('Horizontal ship gets placed correctly if on row other than 1', () => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[16], 4, 'h');
-        let testedCells = [];
-        for(let i = 16; i < 20; i++) {
-            testedCells.push(testBoard.getCells()[i]);
-        }
-        expect(testedCells.every(cell => cell.occupied === true)).toBe(true);
-    })
-
-    test('Horizontal ship will not fit if an occupied cell is in the way', () => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[6], 4, 'h');
-        testBoard.attemptPlaceShip(testBoard.getCells()[5], 4, 'h');
-
-        expect(testBoard.getShips().length).toBe(1);
-    });
-
-    test('Horizontal ship does not get placed when no room: no change', () => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[9], 4, 'h');
-        expect(testBoard.getShips().length).toBe(0);
-    });
-
-    // VERTICAL
-    // shipFits was hidden 
-    /* test('Function tests whether a ship will fit vertically based on size', () => {
-        expect(testBoard.shipFits(testBoard.cells[0], 4, 'v')).toBe(true);
-    });
-    test('Ship will not fit vertically if there is not enough space', () => {
-        expect(testBoard.shipFits(testBoard.cells[90], 4, 'v')).toBe(false);
-    }); */
-
-    test('attemptPlaceShip returns false if there is no room (vertically)', () => {
-        expect(testBoard.attemptPlaceShip(testBoard.getCells()[90], 4, 'v')).toBe(false);
-    });
-
-    test('Ship gets placed vertically on coordinates if there is room: cells become occupied', () => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[0], 4, 'v');
-        let testedCells = [];
-        for(let i = 0; i < 10*4; i+=10) {
-            testedCells.push(testBoard.getCells()[i]);
-        }
-        expect(testedCells.every(cell => cell.occupied === true)).toBe(true);
-    });
-
-    test('Vertical ship will not fit if an occupied cell is in the way', () => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[0], 4, 'v');
-        testBoard.attemptPlaceShip(testBoard.getCells()[10], 4, 'v');
-
-        expect(testBoard.getShips().length).toBe(1);
-    });
-
-    test('Vertical ship does not get placed when no room: no change', () => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[90], 4, 'v');
-        expect(testBoard.getShips().length).toBe(0);
-    });
-
+test('Tracking hit', () => {
+    expect(board1.receiveAttack([3, 3])).toBe('Hit');
 });
 
-describe('Attacking cells', () => {
-
-    test('Attempting an attack marks the targeted cell as attempted', () => {
-        testBoard.receiveAttack(testBoard.getCells()[0]);
-        expect(testBoard.getCells()[0].attempted).toBe(true);
-    });
-
-    test('receiveAttack returns "already attempted" if the targeted cell was already attempted', () => {
-        testBoard.receiveAttack(testBoard.getCells()[0]);
-        expect(testBoard.receiveAttack(testBoard.getCells()[0])).toBe('already attempted');
-    });
-    
-    test('receiveAttack marks ship as hit when targeted position was a hit', () => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[0], 4, 'h');
-        testBoard.receiveAttack(testBoard.getCells()[0]);
-        expect(testBoard.getShips()[0].hits).toStrictEqual([0]);
-    });
-
-    test('No hit adds missed attempt to array', () => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[0], 4, 'h');
-        testBoard.receiveAttack(testBoard.getCells()[10]);
-        expect(testBoard.getMisses()).toStrictEqual([10]);
-    }); 
+test('Tracking miss', () => {
+    expect(board1.receiveAttack([5, 5])).toBe('Miss');
 });
 
-describe('Storing ship data', () => {
-
-    test('Successfully placed vertical ship is stored in an array', () => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[6], 4, 'v');
-        expect(testBoard.getShips().length).toBe(1);
-    });
-
-    test('Successfully placed horizontal ship is stored in an array', () => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[6], 4, 'h');
-        expect(testBoard.getShips().length).toBe(1);
-    });
-
-    test('Horizontal ship coordinates are stored correctly', () => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[6], 4, 'h');
-        expect(testBoard.getShips()[0].positions).toStrictEqual([6, 7, 8, 9]);
-    });
-
-    test('Vertical ship coordinates are stored correctly', () => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[6], 4, 'v');
-        expect(testBoard.getShips()[0].positions).toStrictEqual([6, 16, 26, 36]);
-    });
-});
-
-describe('Game lost condition', () => {
-    beforeEach(() => {
-        testBoard.attemptPlaceShip(testBoard.getCells()[0], 2, 'h');
-    });
-
-    test('allSunk returns false when there is an unsunk ship', () => {
-        expect(testBoard.allSunk()).toBe(false);
-    });
-
-    test('allSunk returns true when all ships are sunk', () => {
-        testBoard.receiveAttack(testBoard.getCells()[0]);
-        testBoard.receiveAttack(testBoard.getCells()[1]);
-        expect(testBoard.allSunk()).toBe(true);
-    });
+test('Tracking if sunk', () => {
+    expect(board1.allSunk).toBe(false);
 });
